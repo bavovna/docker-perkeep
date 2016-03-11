05f0aa7f3742820b864f5ac51152edc6f89041b2
Max Korenkov <mkorenkov@gmail.com>
2016-03-10 22:38:40 -0800

    git clone inside of build container

--- build.sh
+++ build.sh
@@ -3,16 +3,6 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-CAMLISTORE_GIT_URL="https://camlistore.googlesource.com/camlistore"
-CAMLISTORE_RELEASE=0.9
-
-if [ ! -d "build/src/camlistore" ]; then
-  git clone $CAMLISTORE_GIT_URL build/src/camlistore
-  cd build/src/camlistore
-  git checkout tags/$CAMLISTORE_RELEASE
-  cd -
-fi
-
 cd build
 docker build -t mkorenkov/camlistore-build:latest .
 docker run --rm -v `pwd`/bin:/opt/camlistore/bin mkorenkov/camlistore-build:latest run make.go
